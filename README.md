c15a7ae52f19e867abef0378201d0c70988014f9
Sohaib Alqasem <sohaib_alqasem@yahoo.com>
2020-09-14 01:57:11 +0300

    - Add gif demo - Update Readme - Update footer text

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 demo app about using animations libraries with reactJs.
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Demo
+
+![react-animation-example - Animated gif demo](demo/react-animation-repo.gif)
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,7 +12,7 @@ function Footer(props) {
     
         <div className="container">
           <div className="credits">
-            Designed by <a href="/#">Sohaib Alqasem</a>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </div>
         </div>
       </footer>
